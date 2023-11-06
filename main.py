853e7009eaaff5330bdbb4707ffa0bd688946f0e
Olga Cinkova <cinkovaolga@gmail.com>
2023-11-06 15:25:48 +0100

    kalkulacka

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from operace import *
 a = 2
 b = 5
 print(multi(a,b))
+print(uloz_posledni_vysledek())
 print(plus(a,b))
+print(uloz_posledni_vysledek())
+print(vrat_pamet())
+print(pricti_k_pameti(5))
+print(vynasob_s_pameti(1))
--- operace.py
+++ operace.py
@@ -1,8 +1,36 @@
-
+pv = 0
+pamet = 0
 def multi(x:float, y:float)-> float:
     v = x*y
+    global pv
+    pv = v
     return v
 
 def plus(x:float, y:float)-> float:
     v = x+y
+    global pv
     return v
+
+def uloz_posledni_vysledek():
+    global pv
+    return pv
+
+def vrat_pamet():
+    global pamet
+    return pamet
+
+def pricti_k_pameti(op:float):
+    global pamet
+    pamet += op
+    return pamet
+
+def vynasob_s_pameti(op:float):
+    global pamet 
+    pamet = pamet*op
+    return pamet
+
+
+
+    
+
+
